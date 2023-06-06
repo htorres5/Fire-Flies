fd9160a75a50fdeecafee4feb1c5f5c84903fd2f
htorres5 <99515061+htorres5@users.noreply.github.com>
2023-06-06 14:56:11 -0700

    added collider under bridge

--- src/Overworld.js
+++ src/Overworld.js
@@ -7,13 +7,9 @@ class Overworld extends Phaser.Scene {
 
    preload() {
       this.load.path = './assets/'
-      this.load.spritesheet('slime', 'slime.png', {
-         frameWidth: 16,
-         frameHeight: 16,
-      })
-      this.load.image('tilesetImage', 'tileset.png')
-      this.load.image('change_depth', 'change_depth.png')
-      this.load.tilemapTiledJSON('tilemapJSON','prologue_map.json')
+      this.load.image('riku', '/sprites/riku.png')
+      this.load.image('tilesetImage', '/tilemaps/tileset.png')
+      this.load.tilemapTiledJSON('tilemapJSON','/tilemaps/prologue_map.json')
    }
 
    tile(coord) {
@@ -26,11 +22,12 @@ class Overworld extends Phaser.Scene {
 
       // add layer
       this.elevationLayer = map.createLayer('elevation', tileset, 0, 0).setDepth(-1);
-      this.riverLayer = map.createLayer('river', tileset, 0, 0).setDepth(-1)
-      this.bgLayer = map.createLayer('background', tileset, 0, 0)
-      this.pathsLayer = map.createLayer('paths', tileset, 0, 0)
-      this.bridgeLayer = map.createLayer('bridge', tileset, 0, 0)
-      this.decorationsLayer = map.createLayer('decorations', tileset, 0, 0)
+      this.riverLayer = map.createLayer('river', tileset, 0, 0).setDepth(-1);
+      this.underLayer = map.createLayer('under', tileset, 0, 0).setDepth(0);
+      this.bgLayer = map.createLayer('background', tileset, 0, 0);
+      this.pathsLayer = map.createLayer('paths', tileset, 0, 0);
+      this.bridgeLayer = map.createLayer('bridge', tileset, 0, 0);
+      this.decorationsLayer = map.createLayer('decorations', tileset, 0, 0);
       this.housesLayer = map.createLayer('houses', tileset, 0, 0).setDepth(0);
       this.treesLayer = map.createLayer('trees', tileset, 0, 0).setDepth(1);
 
@@ -41,56 +38,57 @@ class Overworld extends Phaser.Scene {
       this.riverArea = this.add.rectangle(this.tile(44), this.tile(20), this.tile(2), this.tile(1), 0x000000, 0.2 ).setOrigin(0);
       this.physics.add.existing(this.riverArea);
 
-      this.underRightBridgeCollider = this.add.rectangle(this.tile(38), this.tile(19), this.tile(3), this.tile(1), 0x000000, 0.2 ).setOrigin(0);
+      this.underRightBridgeCollider = this.add.rectangle(this.tile(37), this.tile(19), this.tile(5), this.tile(1), 0x000000, 0.2 ).setOrigin(0);
       this.physics.add.existing(this.underRightBridgeCollider, true);
 
       // add player
-      this.slime = this.physics.add.sprite(32*57, 32*40, 'slime', 0).setDepth(0);
+      this.riku = this.physics.add.sprite(this.tile(57), this.tile(40), 'riku', 0).setDepth(0);
 
 
-      this.anims.create({
-         key: 'jiggle',
-         frameRate: 8,
-         repeat: -1,
-         frames: this.anims.generateFrameNumbers('slime', {
-            start: 0,
-            end: 1
-         })
-      })
-      this.slime.play('jiggle')
+      // this.anims.create({
+      //    key: 'jiggle',
+      //    frameRate: 8,
+      //    repeat: -1,
+      //    frames: this.anims.generateFrameNumbers('riku', {
+      //       start: 0,
+      //       end: 1
+      //    })
+      // })
+      // this.riku.play('jiggle')
 
-      this.slime.body.setCollideWorldBounds(true)
+      this.riku.body.setCollideWorldBounds(true)
 
       // enable collision
       this.riverLayer.setCollisionByProperty({ collides: true })
+      this.underLayer.setCollisionByProperty({ collides: true })
       this.pathsLayer.setCollisionByProperty({ collides: true })
       this.housesLayer.setCollisionByProperty({ collides: true })
       this.treesLayer.setCollisionByProperty({ collides: true })
       this.bridgeLayer.setCollisionByProperty({ collides: true})
-      this.physics.add.collider(this.slime, this.riverLayer)
-      this.physics.add.collider(this.slime, this.pathsLayer)
-      this.physics.add.collider(this.slime, this.housesLayer)
-      this.physics.add.collider(this.slime, this.treesLayer)
+      this.physics.add.collider(this.riku, this.riverLayer)
+      this.physics.add.collider(this.riku, this.pathsLayer)
+      this.physics.add.collider(this.riku, this.housesLayer)
+      this.physics.add.collider(this.riku, this.treesLayer)
 
       this.isInRiverLayer = false;
       // * Bridge Collision
-      this.bridgeCollider = this.physics.add.collider(this.slime, this.bridgeLayer)
+      this.bridgeCollider = this.physics.add.collider(this.riku, this.bridgeLayer)
 
-      this.underRightBridgeCollider = this.physics.add.collider(this.slime, this.underRightBridgeCollider)
-      this.underRightBridgeCollider.active = false;
+      this.underCollider = this.physics.add.collider(this.riku, this.underLayer)
+      this.underCollider.active = false;
 
-      this.riverAreaCollider = this.physics.add.overlap(this.slime, this.riverArea, () => {
+      this.riverAreaCollider = this.physics.add.overlap(this.riku, this.riverArea, () => {
          this.isInRiverLayer = true;
          console.log(`Collider callback function: ${this.isInRiverLayer}`)
       })
-      this.regularAreaCollider = this.physics.add.overlap(this.slime, this.regularArea, () => {
+      this.regularAreaCollider = this.physics.add.overlap(this.riku, this.regularArea, () => {
          this.isInRiverLayer = false;
          console.log(`Collider callback function: ${this.isInRiverLayer}`)
       })
 
       // cameras
       this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-      this.cameras.main.startFollow(this.slime, true, 0.25, 0.25)
+      this.cameras.main.startFollow(this.riku, true, 0.25, 0.25)
       this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels)
 
       // input
@@ -111,19 +109,19 @@ class Overworld extends Phaser.Scene {
       }
 
       if(this.isInRiverLayer) {
-         this.slime.setDepth(-1);
-         console.log(this.slime.depth)
-         this.underRightBridgeCollider.active = true;
+         this.riku.setDepth(-1);
+         console.log(this.riku.depth)
+         this.underCollider.active = true;
          this.bridgeCollider.active = false;
       } else {
-         this.slime.setDepth(1);
-         this.underRightBridgeCollider.active = false;
+         this.riku.setDepth(1);
+         this.underCollider.active = false;
          this.bridgeCollider.active = true;
       }
 
       console.log(`update function: ${this.isInRiverLayer}`)
 
       this.direction.normalize();
-      this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y);
+      this.riku.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y);
    }
 }
